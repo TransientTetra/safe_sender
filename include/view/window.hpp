37f03110750f33e6e37b04b6b12c3525e24b18d0
msperkow <sperkowski.mikolaj@gmail.com>
2020-03-06 22:45:51 +0100

    Add window class, update cmakelists

---
+++ include/view/window.hpp
@@ -0,0 +1,17 @@
+//
+// Created by mkj on 3/6/20.
+//
+
+#ifndef SAFE_SENDER_WINDOW_HPP
+#define SAFE_SENDER_WINDOW_HPP
+
+
+class Window
+{
+private:
+protected:
+public:
+};
+
+
+#endif //SAFE_SENDER_WINDOW_HPP
